81907cf833c728dfc5c6083edfaaa8f362488303
Giorgio Aquino <gigetto@gmail.com>
2016-08-25 00:05:33 +0200

    Try

--- client/components/DailyList.js
+++ client/components/DailyList.js
@@ -2,47 +2,29 @@ import React, { Component } from 'react';
 
 import { Link } from 'react-router';
 
-import {Button, IconButton} from 'react-toolbox/lib/button';
+import { Button } from 'react-toolbox/lib/button';
 
 import { List } from 'react-toolbox/lib/list';
 
 import Activity from './Activity';
 
 class DailyList extends Component {
+
   render() {
-    const { addActivity, activities } = this.props;
+    const { activities } = this.props;
 
     return (<div>
 
-        <List>
-          {activities.map(activity => <Activity key={activity.id} {...activity} />)}
-        </List>
+      <List>
+        {activities.map(activity => <Activity key={activity.id} {...activity} />)}
+      </List>
 
-        <Link to="/add">
-          <Button icon='add' floating />
-        </Link>
-      </div>
+      <Link to="/add">
+        <Button icon="add" floating />
+      </Link>
+    </div>
     );
   }
 }
-
-// let DailyList = (props) => {
-//   return {
-//     render() {
-//         const { addActivity, activities } = props;
-//
-//         return (<div>
-//             <FloatingActionButton onClick={addActivity}>
-//               <ContentAdd />
-//             </FloatingActionButton>
-//
-//             <List>
-//               {activities.map(activity => <Activity key={activity.id} {...activity} />)}
-//             </List>
-//           </div>
-//         );
-//       }
-//   }
-// };
 
 export default DailyList;
--- client/components/Main.js
+++ client/components/Main.js
@@ -10,19 +10,18 @@ const Main = (props) => {
   return {
 
     render() {
-      return (<Layout>
-
+      return (<div>
+{/*
+      <Layout>
       <Panel>
             <AppBar><IconButton icon="menu" inverse onClick={this.toggleDrawerActive} /></AppBar>
             <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
-                {React.cloneElement(props.children, props)}
             </div>
-            {/* We use cloneElement here so we can auto pass down props */}
-
         </Panel>
-
-
-    </Layout>);
+    </Layout>);*/}
+    {/* We use cloneElement here so we can auto pass down props */}
+    {React.cloneElement(props.children, props)}
+    </div>)
     },
     toggleDrawerActive() {
 
