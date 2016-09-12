6d56aaa68532d1de6a8ebae17767e21d81dd826d
Giorgio Aquino <gigetto@gmail.com>
2016-09-12 06:28:49 +0200

    Updates

--- sw.js
+++ sw.js
@@ -12,6 +12,31 @@ const Notify = {
   send() {
     return self.registration.showNotification('Hi folks', { body: 'Nice to meet you' });
   },
+
+  activate(notification) {
+
+    console.log(notification);
+
+    this.notification = notification;
+
+    this.ST = setTimeout(() => {
+      self.registration.showNotification(notification.title, notification.config);
+      this.clear();
+    }, notification.timeout);
+
+    return this
+  },
+
+  getNotification() {
+    return this.notification;
+  },
+
+  clear() {
+    this.notification = null;
+    clearTimeout(this.ST);
+    return this;
+  },
+
 };
 
 self.addEventListener('push', function (event) {
@@ -31,27 +56,38 @@ self.addEventListener('push', function (event) {
 
 });
 
-// self.addEventListener('message', function (event) {
-//   // do something with the message event.data
-//   console.log(event);
-//
-//   switch (event.data.command) {
-//
-//     case 'SET_NOTIFICATION':
-//       Notify.send(event.data.options);
-//       break;
-//
-//     case 'CLEAR_NOTIFICATION':
-//       Notify.clear();
-//       break;
-//   }
-//
-//   // console.log(Notification);
-//
-//   // event.showNotification('Nice').then((e) => {
-//   //   console.log(e);
-//   // }).catch(function (err) {console.log(err)});
-// });
+self.addEventListener('message', function (event) {
+  // do something with the message event.data
+  console.log(event);
+
+  switch (event.data.command) {
+
+    case 'SET_NOTIFICATION':
+      Notify.clear().activate(event.data.options);
+
+        event.ports[0].postMessage({
+          type: 'NOTIFICATION_SET'
+        });
+      break;
+
+    case 'CLEAR_NOTIFICATION':
+      Notify.clear();
+      event.ports[0].postMessage({
+        type: 'NOTIFICATION_CLEARED'
+      });
+      break;
+
+    case 'GET_NOTIFICATION':
+        event.ports[0].postMessage(Notify.getNotification());
+      break;
+  }
+
+  // console.log(Notification);
+
+  // event.showNotification('Nice').then((e) => {
+  //   console.log(e);
+  // }).catch(function (err) {console.log(err)});
+});
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting()); // Activate worker immediately
